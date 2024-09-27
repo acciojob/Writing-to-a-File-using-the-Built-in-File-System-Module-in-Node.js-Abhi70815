6f2ae060a2b99007665c6aaef1fb9336c10970cd
Abhi70815 <abhishekchurasia45621@gmail.com>
2024-09-28 00:54:43 +0530

    save

--- main.js
+++ main.js
@@ -2,3 +2,11 @@ const fs = require('fs');
 
 // TODO: Write the message "Hello, World!" to the file "output.txt"
 
+fs.writeFile('./output.txt',"Hello, World!",(err)=>{
+    if(err){
+        console.log(err)
+    }
+    else{
+        console.log("success")
+    }
+})
